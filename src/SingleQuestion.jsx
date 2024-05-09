72984e6c49e55a217aa4d44d156e5377dd547c14
Amit <amitsinghyadav974@gmail.com>
2024-05-10 01:13:23 +0530

    some Changes

--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -10,8 +10,8 @@ let SingleQuestion = ({ question }) => {
 
     return (
         <section className="shadow-lg bg-slate-200 my-5">
-            <div className=" h-[auto] py-5 px-3 flex items-center justify-between">
-                <h2 className="text-2xl pl-2">{title}</h2>
+            <div className=" h-[auto] py-5 px-3 flex items-center justify-between ">
+                <h2 className="text-2xl pl-2  w-[85%]">{title}</h2>
                 <button type="button" className=" text-2xl bg-blue-400 w-[30px] h-[30px] rounded-[50%] text-white flex justify-center items-center" onClick={() => { setShowInfo(!showInfo) }}>{showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}</button>
             </div>
             {showInfo && <p className="text-[1.2rem] p-3">{info}</p>}
